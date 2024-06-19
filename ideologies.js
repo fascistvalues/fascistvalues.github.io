60b722bd22a441dfc3b2dd732abb0bfc72a597cf
C4brit4 <jlopfez34@gmail.com>
2024-06-19 20:29:45 +0200

    set ideologies and lang folder

--- ideologies.js
+++ ideologies.js
@@ -1,119 +1,110 @@
 const ideologies = { // eslint-disable-line no-unused-vars
-  'Marxism-Leninism': {
-    a: 80,
-    b: 80,
-    c: 80,
-    d: 60,
-    e: 90,
-    f: 70,
-    g: 40
-  },
-  'Orthodox-Marxism': {
-    a: 80,
-    b: 80,
-    c: 60,
-    d: 80,
-    e: 70,
-    f: 50,
-    g: 30
-  },
-  'Eco-Marxism': {
-    a: 80,
-    b: 80,
-    c: 60,
-    d: 80,
-    e: 70,
-    f: 20,
-    g: 30
-  },
-  'Centrist-Marxism': {
+  'Italian-Fascism': {
     a: 50,
-    b: 80,
-    c: 60,
-    d: 60,
-    e: 70,
-    f: 50,
-    g: 30
-  },
-  'Council-Communism': {
-    a: 100,
-    b: 70,
-    c: 40,
-    d: 80,
-    e: 50,
-    f: 50,
-    g: 30
-  },
-  'Left-Communism': {
-    a: 90,
     b: 60,
     c: 50,
-    d: 90,
-    e: 80,
+    d: 30,
+    e: 30,
     f: 50,
-    g: 30
-  },
-  'Anarcho-Communism': {
-    a: 80,
-    b: 50,
-    c: 20,
-    d: 80,
-    e: 10,
-    f: 50,
-    g: 30
-  },
-  'Eco-Anarchism': {
-    a: 80,
-    b: 50,
-    c: 20,
-    d: 80,
-    e: 10,
-    f: 20,
-    g: 30
-  },
-  'Market-Anarchism': {
-    a: 20,
-    b: 40,
-    c: 30,
-    d: 70,
-    e: 10,
-    f: 60,
-    g: 30
-  },
-  'Utopian-Socialism': {
-    a: 10,
-    b: 10,
-    c: 40,
-    d: 20,
-    e: 50,
-    f: 40,
     g: 50
   },
-  'Democratic-Socialism': {
-    a: 20,
-    b: 30,
-    c: 30,
-    d: 70,
-    e: 60,
-    f: 30,
-    g: 20
+  'National-Socialism': {
+    a: 30,
+    b: 80,
+    c: 90,
+    d: 20,
+    e: 20,
+    f: 40,
+    g: 70
   },
-  'Social-Democracy': {
-    a: 10,
-    b: 30,
-    c: 50,
-    d: 40,
-    e: 80,
+  'British-Fascism': {
+    a: 60,
+    b: 40,
+    c: 30,
+    d: 80,
+    e: 40,
     f: 50,
     g: 30
   },
-  'Left-Wing-Nationalism': {
+  'Falangism': {
+    a: 80,
+    b: 10,
+    c: 10,
+    d: 40,
+    e: 60,
+    f: 50,
+    g: 30
+  },
+  'Esoteric Fascism': {
+    a: 50,
+    b: 90,
+    c: 90,
+    d: 10,
+    e: 10,
+    f: 70,
+    g: 80
+  },
+  'Integralism': {
     a: 60,
+    b: 30,
+    c: 20,
+    d: 50,
+    e: 90,
+    f: 30,
+    g: 70
+  },
+  'Clerical Fascism': {
+    a: 50,
+    b: 60,
+    c: 60,
+    d: 60,
+    e: 90,
+    f: 60,
+    g: 80
+  },
+  'Eco-Fascism': {
+    a: 50,
+    b: 90,
+    c: 90,
+    d: 10,
+    e: 10,
+    f: 10,
+    g: 80
+  },
+  'Strasserism': {
+    a: 80,
+    b: 40,
+    c: 70,
+    d: 50,
+    e: 50,
+    f: 60,
+    g: 40
+  },
+  'Homofascism': {
+    a: 40,
+    b: 80,
+    c: 70,
+    d: 20,
+    e: 30,
+    f: 50,
+    g: 10
+  },
+  'Silver-Legionism': {
+    a: 20,
     b: 60,
     c: 60,
     d: 30,
-    e: 70,
-    f: 40,
-    g: 30
+    e: 60,
+    f: 70,
+    g: 70
+  },
+  'Alt-Right': {
+    a: 30,
+    b: 70,
+    c: 90,
+    d: 40,
+    e: 50,
+    f: 70,
+    g: 90
   }
 }
